947f8fd3430864b517675a45ab1317901fb3f5b9
TuanPA <chutieuk7i@gmail.com>
2018-07-10 20:40:52 +0700

    r-way-tries basic

---
+++ r-way-tries/r-way-tries.go
@@ -0,0 +1,55 @@
+package r_way_tries
+
+import "log"
+
+type Node struct {
+	value interface{}
+	next  map[string]*Node
+}
+
+var root = &Node{"", make(map[string]*Node)}
+var size int
+
+func Put(key string, val string) {
+	if key == "" {
+		log.Fatal("Key not null");
+	}
+	put(root, key, val, 0)
+}
+
+func Get(key string) interface{} {
+	if key == "" {
+		log.Fatal("Key not null");
+	}
+
+	nodeResult := get(root, key, 0)
+	return nodeResult.value
+}
+
+func Size() int  {
+	return size
+}
+
+func get(node *Node, key string, d int) *Node {
+	if d == len(key) {
+		return node
+	}
+	c := string(key[d])
+	return get(node.next[c], key, d+1)
+}
+
+func put(node *Node, key string, val string, d int) *Node {
+	if d == len(key) {
+		if val == "" {
+			size++
+		}
+		node.value = val
+		return node
+	}
+	c := string(key[d])
+	if _, ok := node.next[c]; !ok {
+		node.next[c] = &Node{"", make(map[string]*Node)}
+	}
+	put(node.next[c], key, val, d+1)
+	return node
+}
---
+++ r-way-tries/r-way-tries_test.go
@@ -0,0 +1,11 @@
+package r_way_tries
+
+import (
+	"testing"
+)
+func TestInsert(t *testing.T) {
+	Put("Tuanpa", "xTuanpa");
+	Put("Tuanpaa", "xxx");
+	Put("Pi", "Tuanpa");
+	Put("Pia", "Tuanpa");
+}
